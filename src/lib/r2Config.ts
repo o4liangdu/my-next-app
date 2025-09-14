e694b846e2e39e8482dfda4b3b297eda1826039e
liangdu <liangdo18@qq.com>
2025-09-14 18:22:04 +0800

    feat: env

--- src/lib/r2Config.ts
+++ src/lib/r2Config.ts
@@ -1,9 +1,7 @@
 // Cloudflare R2 Configuration
 export const r2Config = {
-  // token
-  // token: 'Yy-cTS10jJ-1U_fm80TIYCh8nnN7JrY5sBbku3C0',
-  accountId: '9a7fb883ca03fc69c26b423a8b23ff75',
-  accessKeyId: '44ac249349ed19d4788ea72865975d7e',
-  secretAccessKey: 'dbe63684b45d007d6156970a57799dc7aa85c33f18efb8f18ed793e56d67e11a',
-  bucketName: 'cfr2-videos'
+  accountId: process.env.CLOUDFLARE_ACCOUNT_ID || '',
+  accessKeyId: process.env.R2_ACCESS_KEY_ID || '',
+  secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || '',
+  bucketName: process.env.R2_BUCKET_NAME || 'videos'
 };
