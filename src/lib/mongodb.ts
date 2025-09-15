d622dee2846cd164ef9b3764bcef174e75852ed6
liangdu <liangdo18@qq.com>
2025-09-15 18:29:41 +0800

    feat: eslint fix

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -9,7 +9,7 @@ let clientPromise: Promise<MongoClient>;
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the MongoClient
   // is not re-created for every request
-  let globalWithMongo = global as typeof globalThis & {
+  const globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>;
   };
 
